5b204e059ced67323a39f52f4d7005550faaf2af
Виктор Иванов <vonavitya@gmail.com>
2023-11-07 05:07:36 +0500

    Добавьте файлы проекта.

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
